2b9fd8653f6d593b4fa3c0c3e648bee7f955fe81
Truewarlock <44729871+Truewarlock@users.noreply.github.com>
2020-07-11 10:53:08 +0300

    Add files via upload

---
+++ ProductionCode/Greeting.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1.ProductionCode
+{
+    internal class Greeting
+    {
+        public Greeting()
+        {
+        }
+        internal string Greet(string[] name)
+        {
+            if ((name == null) || (name.Length == 0))
+            {
+                return "Hello, my friend.";
+            }
+            if (name.GetLength(0) == 1)
+            {
+                if (name[0] == name[0].ToUpper())
+                {
+                    return $"HELLO, {name[0].ToUpper()}!";
+                }
+                return $"Hello, {name[0]}.";
+            }
+            else {
+                string aux = "Hello, ";
+                string[] upper = new string[name.GetLength(0)];
+                int upprNr = 0;
+                for (int i = 0; i < name.GetLength(0); i++)
+                {
+                    if (name[i]== name[i].ToUpper())
+                    {
+                        upper[upprNr] = name[i];
+                        upprNr += 1;
+                    }
+                    else {
+                        if (i == 0)
+                        {
+                            aux = aux + name[i];
+                        }
+                        if ((i == name.GetLength(0) - 1))
+                        {
+                            aux = aux + " and " + name[i];
+                        }
+                        if ((i != 0) && (i != name.GetLength(0) - 1))
+                        {
+                            aux = aux + ", " + name[i];
+                        }
+                    }
+
+                }
+                if (upprNr > 0)
+                { aux = aux + ".";
+                    for (int i = 0; i<upprNr;i++)
+                        {
+                        aux = aux + " AND " + upper[i];
+                        }
+                    aux = aux + "!";
+                }
+                
+                return aux;
+                
+            }
+            return "Ce naiba nu merge";
+        }
+
+    }
+
+
+}
+
+
---
+++ Tests/GreetingTest.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using Seminar1.ProductionCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar1.Tests
+{
+    [TestFixture]
+    class GreetingTest
+    {
+        Greeting greeting = new Greeting();
+
+        public string[] GenerateName(string par)
+        {
+            string[] test = { "kek" };
+            test[0] = par;
+            return test;
+        }
+
+        public void ExecuteTest(string[] name, string expected)
+        {
+            //Arrange
+
+            //Act
+            string result = greeting.Greet(name);
+            //Assert
+            Console.WriteLine(result);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void ShouldGreetBob()
+        {
+            ExecuteTest(GenerateName("Bob"), "Hello, Bob.");
+        }
+        [Test]
+        public void ShouldHandleNull()
+        {
+
+            ExecuteTest(null, "Hello, my friend.");
+        }
+        [Test]
+        public void ShouldGreetGigi()
+        {
+
+            ExecuteTest(GenerateName("Gigi"), "Hello, Gigi.");
+        }
+        [Test]
+        public void ShouldHandleShouting()
+        {
+
+            ExecuteTest(GenerateName("JERRY"), "HELLO, JERRY!");
+        }
+        [Test]
+        public void ShoulHanndleTwo() {
+
+            string[] test = { "Jill", "Jane" };
+            ExecuteTest(test,"Hello, Jill and Jane");
+        }
+
+        [Test]
+        public void ShoulHanddleMultiple()
+        {
+            string[] test = { "Amy", "Brian", "Charlotte" };
+            ExecuteTest(test, "Hello, Amy, Brian and Charlotte");
+        }
+        [Test]
+        public void ShoulHanddleMultipleAndShouting()
+        {
+            string[] test = { "Amy", "BRIAN", "Charlotte" };
+            ExecuteTest(test, "Hello, Amy and Charlotte. AND BRIAN!");
+        }
+
+
+
+    }
+}
